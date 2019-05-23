a12088a7dd8e5b58679ad825ebdc1a3bb7ace026
Jakub2801 <469367@fi.muni.cz>
2019-05-23 19:55:35 +0200

    store working validation dont

--- src/main/java/com/muni/fi/pb138/StoreProcessor.java
+++ src/main/java/com/muni/fi/pb138/StoreProcessor.java
@@ -1,6 +1,7 @@
 package com.muni.fi.pb138;
 
 import net.xqj.basex.BaseXXQDataSource;
+import net.xqj.basex.bin.L;
 import org.xml.sax.SAXException;
 
 import javax.xml.XMLConstants;
@@ -14,6 +15,11 @@ import javax.xml.xquery.XQException;
 import javax.xml.xquery.XQPreparedExpression;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
+import java.util.stream.Collectors;
 
 /**
  * @author Jakub Petras
@@ -22,21 +28,22 @@ import java.io.IOException;
 public class StoreProcessor implements Processor {
     private XQDataSource xqDataSource = new BaseXXQDataSource();
     private XQConnection connection;
-    private static final String xsdPath = "/Users/Jakub2801/Documents/MojePrace/MU/2019-jar/" +
-                                        "PB138/EuropassProject/PB138-projekt/src/main/resources/" +
-                                        "europass-xml-schema-definition-v3/EuropassSchema.xsd";
+    private static final String xsdPath = "europass-xml-schema-definition-v3/EuropassSchema_V3.0.xsd";
 
     private void setUpDB() {
         try {
-            xqDataSource.setProperty("serverName", "localhost");
-            xqDataSource.setProperty("port", "1984");
-            xqDataSource.setProperty("user", "admin");
-            xqDataSource.setProperty("password", "admin");
-            xqDataSource.setProperty("databaseName", "europassDB");
+            Properties properties = new Properties();
+            properties.load(Main.class.getResourceAsStream("/dbSetup.properties"));
+
+            xqDataSource.setProperty("serverName", properties.getProperty("serverName"));
+            xqDataSource.setProperty("port", properties.getProperty("port"));
+            xqDataSource.setProperty("user", properties.getProperty("user"));
+            xqDataSource.setProperty("password", properties.getProperty("password"));
+            xqDataSource.setProperty("databaseName", properties.getProperty("databaseName"));
 
 
             connection = xqDataSource.getConnection("admin", "admin");
-        } catch (XQException e) {
+        } catch (XQException | IOException e) {
             System.err.println("Can not connect to DB");
             e.printStackTrace();
             System.exit(1);
@@ -49,7 +56,28 @@ public class StoreProcessor implements Processor {
         expression.executeQuery();
     }
 
-    private void validateXML(String path, int i) {
+    private static StreamSource[] generateStreamSourcesFromXsdPaths(
+            final String[] xsdFilesPaths) {
+        return Arrays.stream(xsdFilesPaths)
+                .map(StreamSource::new)
+                .collect(Collectors.toList())
+                .toArray(new StreamSource[xsdFilesPaths.length]);
+    }
+
+    private String[] getAllXsdPaths(String directory, List<String> paths) {
+        File[] files = new File(directory).listFiles();
+        for(File file : files){
+            if(file.isFile()){
+                //System.out.println(file.getAbsolutePath());
+                paths.add(file.getAbsolutePath());
+            } else {
+                getAllXsdPaths(file.getAbsolutePath(), paths);
+            }
+        }
+        String[] output = new String[paths.size()];
+        return paths.toArray(output);
+    }
+    private void validateXML(String path) {
         try {
             SchemaFactory factory =
                     SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
@@ -57,7 +85,7 @@ public class StoreProcessor implements Processor {
             Validator validator = schema.newValidator();
             validator.validate(new StreamSource(new File(path)));
         } catch (IOException | SAXException e) {
-            System.err.println("Validation failed! " + i);
+            System.err.println("Validation failed! in: " + path);
             e.printStackTrace();
             System.exit(3);
         }
@@ -68,7 +96,7 @@ public class StoreProcessor implements Processor {
         setUpDB();
         try {
             for (int i = 0; i < args.length; i++) {
-                validateXML(args[i], i);
+                //TODO validateXML(args[i]);
                 executeXQuery(args[i]);
             }
             connection.close();
